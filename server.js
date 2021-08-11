a55ecd7b723773d5836b80c61bf54b94dbf83e7b
Eugen Kuhn <kuhn@sendesignal.de>
2021-08-11 23:19:15 +0200

    change get methode

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.urlencoded({limit: '50mb', extended: true}));
 app.use(express.json( {limit: '50mb', extended: true}));
 
 app.get('/', function(req, res) {
-    res.send('Hello world!');
+    res.send({msg: 'Hello world!'});
     //res.sendFile('index.html');
 });
 
